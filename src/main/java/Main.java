cac0c0784584f8f012e4e8e23b71049642e6088f
0036508924 <luka.lendel@fer.hr>
2019-12-03 10:17:05 +0100

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
 public class Main {
-
     public static void main(String[] args) {
-        // insert
+        StringBuilder result = method(Integer.parseInt(args[0]));
+        System.out.println(result.toString());
+    }
+
+    private static StringBuilder method(Integer n) {
+        StringBuilder result = new StringBuilder();
+
+        for(Integer i = 1; i <= n; i++) {
+            if(i % 3 == 0) result.append("fizz");
+            if(i % 5 == 0) result.append("buzz");
+            if((i % 3 != 0) && (i % 5 != 0)) result.append(i.toString());
+            result.append("\n");
+        }
+
+        return result;
     }
 }
