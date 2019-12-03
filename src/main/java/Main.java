d4d2f208713a7435b329d220457bf96e10ab374c
Bartol Bilić <bartol.bilic1@gmail.com>
2019-12-03 10:15:57 +0100

    Implemented main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
 public class Main {
 
     public static void main(String[] args) {
-        // insert
+        try{
+            int input = Integer.parseInt(args[0]);
+            customPrint(input);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Number is not provided.");
+        }
+    }
+
+    public static void customPrint(int n) {
+        StringBuilder output = new StringBuilder();
+
+        for(int i = 1; i <=n; i++) {
+            if(i%15 == 0) {
+                output.append("fizzbuzz\n") ;
+                continue;
+            }
+
+            if(i%3 == 0) {
+                output.append("fizz\n");
+                continue;
+            }
+
+            if(i%5 == 0) {
+                output.append("buzz\n");
+                continue;
+            }
+            output.append(i);
+            output.append("\n");
+        }
+        System.out.println(output);
     }
 }
