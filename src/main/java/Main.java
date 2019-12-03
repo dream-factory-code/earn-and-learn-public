53bae8bea5bc814710dd9e323386d780d3cddb49
Mia Jurdana <mia@Vices-MacBook-Pro.local>
2019-12-03 10:59:31 +0100

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,23 @@
 public class Main {
 
     public static void main(String[] args) {
-        // insert
+        fizbuz(Integer.parseInt(args[0]));
+
+
+
+    }
+
+    public static void fizbuz(int x){
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=x;i++){
+            if(i%3==0)
+                sb.append("fizz");
+            if(i%5==0)
+                sb.append("buzz");
+            if(i%3!=0 && i%5!=0)
+                sb.append(i);
+            sb.append("\n");
+            System.out.println(sb);
+        }
     }
 }
