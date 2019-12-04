02fc31a46763d64e54233458ef5d76ed1df39f4a
Mia Jurdana <mia@Vices-MacBook-Pro.local>
2019-12-04 11:53:38 +0100

    nemam pojma sta radim

--- src/main/java/hr/dreamfactory/mia/Main.java
+++ src/main/java/hr/dreamfactory/mia/Main.java
@@ -1,20 +1,21 @@
+package hr.dreamfactory.mia;
+
 public class Main {
 
     public static void main(String[] args) {
-        fizbuz(Integer.parseInt(args[0]));
-
+        fizBuz(Integer.parseInt(args[0]));
 
 
     }
 
-    public static void fizbuz(int x){
+    public static void fizBuz(int x) {
         StringBuilder sb = new StringBuilder();
-        for(int i=1;i<=x;i++){
-            if(i%3==0)
+        for (int i = 1; i <= x; i++) {
+            if (i % 3 == 0)
                 sb.append("fizz");
-            if(i%5==0)
+            if (i % 5 == 0)
                 sb.append("buzz");
-            if(i%3!=0 && i%5!=0)
+            if (i % 3 != 0 && i % 5 != 0)
                 sb.append(i);
             sb.append("\n");
             System.out.println(sb);
