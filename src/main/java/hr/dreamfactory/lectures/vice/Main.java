c62b39564d04e4dcafd1fa0f2e7e93f0af1265ac
fr33d00m <vice.simunov@gmail.com>
2019-12-04 11:37:55 +0100

    minor style improvements

--- src/main/java/hr/dreamfactory/lectures/vice/Main.java
+++ src/main/java/hr/dreamfactory/lectures/vice/Main.java
@@ -1,35 +1,45 @@
+package hr.dreamfactory.lectures.vice;
+
 public class Main {
 
     public static void main(String[] args) {
-        try{
+        if (args.length < 1) {
+            System.out.println("A parameter should be specified");
+            System.exit(1);
+        }
+
+        int input2 = Integer.parseInt(args[0]);
+
+        try {
             int input = Integer.parseInt(args[0]);
             customPrint(input);
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             System.out.println("Number is not provided.");
         }
-    }
+}
 
     public static void customPrint(int n) {
         StringBuilder output = new StringBuilder();
-        for(int i = 1; i <=n; i++) {
-            if(i%15 == 0) {
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
                 output.append("fizzbuzz\n") ;
                 continue;
             }
 
-            if(i%3 == 0) {
+            if (i % 3 == 0) {
                 output.append("fizz\n");
                 continue;
             }
 
-            if(i%5 == 0) {
+            if (i % 5 == 0) {
                 output.append("buzz\n");
                 continue;
             }
             output.append(i);
             output.append("\n");
         }
+
         System.out.println(output);
     }
 }
