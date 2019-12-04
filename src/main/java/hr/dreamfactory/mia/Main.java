fd014237956e2673d531ea88a6c19de39c14e640
Mia Jurdana <mia@Vices-MacBook-Pro.local>
2019-12-04 12:15:08 +0100

    evo brate, fixan code

--- src/main/java/hr/dreamfactory/mia/Main.java
+++ src/main/java/hr/dreamfactory/mia/Main.java
@@ -3,22 +3,34 @@ package hr.dreamfactory.mia;
 public class Main {
 
     public static void main(String[] args) {
-        fizBuz(Integer.parseInt(args[0]));
-
-
+        if(args.length < 1){
+            System.out.println("argument not provided");
+            System.exit(1);
+        }
+        try{
+            int n = (Integer.parseInt(args[0]));
+            String output = fizBuz(n);
+            System.out.println(output);
+        }
+        catch (NumberFormatException){
+            System.out.println("number not provided");
+        }
     }
 
-    public static void fizBuz(int x) {
+    public static String fizBuz(int x) {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= x; i++) {
-            if (i % 3 == 0)
+            if (i % 3 == 0) {
                 sb.append("fizz");
-            if (i % 5 == 0)
+            }
+            if (i % 5 == 0) {
                 sb.append("buzz");
-            if (i % 3 != 0 && i % 5 != 0)
+            }
+            if (i != 15) {
                 sb.append(i);
+            }
             sb.append("\n");
-            System.out.println(sb);
         }
+        return sb.toString();
     }
 }
