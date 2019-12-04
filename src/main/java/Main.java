47a72e9af3a42c28f1e8494b7ad40df75a69110f
Mario <babicmario@hotmail.com>
2019-12-04 07:47:23 +0100

    firstcommit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        // insert
+        int n = Integer.parseInt(args[0]);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i=0; i <= n; i++){
+            if (i%15==0) {
+                sb.append("fizzbuzz\n");
+            } else if (i%5==0) {
+                sb.append("fizz\n");
+            } else if (i%3==0) {
+                sb.append("buzz\n");
+            } else {
+                sb.append(i);
+            }
+        }
+        System.out.println(sb);
     }
 }
