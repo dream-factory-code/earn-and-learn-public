ca7c7283e05b97b469c64409d2866c3bcd0c682e
Mia Jurdana <mia@Vices-MacBook-Pro.local>
2019-12-10 23:41:33 +0100

    first week complete

---
+++ src/main/java/hr/dreamfactory/lectures/Main.java
@@ -0,0 +1,46 @@
+package hr.dreamfactory.lectures;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            LOGGER.error("Please provide a natural number");
+            System.exit(1);
+        }
+
+        try {
+            int n = Integer.parseInt(args[0]);
+            String output = fizBuz(n);
+            LOGGER.info(output);
+        } catch (NumberFormatException e) {
+            LOGGER.error("Input is not number", e);
+        }
+    }
+
+    public static String fizBuz(int x) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= x; i++) {
+            if (i % 3 == 0) {
+                sb.append("fizz");
+            }
+            if (i % 5 == 0) {
+                sb.append("buzz");
+            }
+            if (i % 3 != 0 && i % 5 != 0) {
+                sb.append(i);
+            }
+            sb.append("\n");
+            if (i % 2 == 0) {
+                LOGGER.debug("iterated over:{}", i);
+            }
+        }
+        return sb.toString();
+    }
+}
+
+
+
---
+++ src/main/java/hr/dreamfactory/lectures/model/Banner.java
@@ -0,0 +1,29 @@
+package hr.dreamfactory.lectures.model;
+
+public class Banner {
+    private String title;
+    private String url;
+    private String id;
+    private String imgUrl;
+    private String recomendationUrl;
+
+    public String getrecomendationUrl() {
+        return recomendationUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
