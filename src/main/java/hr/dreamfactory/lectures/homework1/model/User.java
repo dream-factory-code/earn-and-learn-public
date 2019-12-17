aa0e58dcd3e22fa85e3770258217a2f9a0bfad5f
Karlo Ježić <karlo.jezic@fer.hr>
2019-12-17 10:49:58 +0100

    added new parametars to user

--- src/main/java/hr/dreamfactory/lectures/homework1/model/User.java
+++ src/main/java/hr/dreamfactory/lectures/homework1/model/User.java
@@ -1,12 +1,62 @@
 package hr.dreamfactory.lectures.homework1.model;
 
+import com.google.gson.annotations.SerializedName;
+import com.univocity.parsers.annotations.Nested;
 import com.univocity.parsers.annotations.Parsed;
+
+import javax.ws.rs.Path;
 
 public class User {
 
     private Name name;
 
     private Location location;
+
+    @Parsed
+    private String gender;
+
+    @Parsed
+    private String phone;
+
+    @Parsed
+    @SerializedName("nat")
+    private String nationality;
+
+    @Nested
+    @SerializedName("dob")
+    private Dob date;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Dob getDate() {
+        return date;
+    }
+
+    public void setDate(Dob date) {
+        this.date = date;
+    }
 
     public Name getName() {
         return name;
