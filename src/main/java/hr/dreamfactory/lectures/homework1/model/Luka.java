c2a34ee562b108d0551c0c153433d51096c953f6
fr33d00m <vice.simunov@gmail.com>
2019-12-18 08:42:20 +0100

    try git

---
+++ src/main/java/hr/dreamfactory/lectures/homework1/model/Luka.java
@@ -0,0 +1,11 @@
+package hr.dreamfactory.lectures.homework1.model;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Luka implements Closeable {
+    @Override
+    public void close() throws IOException {
+
+    }
+}
