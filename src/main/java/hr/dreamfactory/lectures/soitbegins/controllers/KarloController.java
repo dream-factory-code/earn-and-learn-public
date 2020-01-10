c26c98946a124456022bdcb92fc896e7aa86f3a1
Karlo Ježić <karlo.jezic@fer.hr>
2020-01-10 10:35:11 +0100

    change in KarloController

--- src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/KarloController.java
+++ src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/KarloController.java
@@ -3,6 +3,8 @@ package hr.dreamfactory.lectures.soitbegins.controllers;
 import hr.dreamfactory.lectures.soitbegins.UserRepository;
 import hr.dreamfactory.lectures.soitbegins.model.users.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -11,7 +13,8 @@ public class KarloController {
     @Autowired
     private UserRepository repository;
 
-    public List<User> filterByCountry(String country){
+    @RequestMapping("/countryUser")
+    public List<User> filterByCountry(@RequestParam String country){
         return repository.find(country);
     }
 }
