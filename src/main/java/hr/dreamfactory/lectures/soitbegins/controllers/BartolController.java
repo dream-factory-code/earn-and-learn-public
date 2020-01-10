8b615e92090bd365e1c5e677e1c94f0e4d828b71
Bartol Bilić <bartol.bilic1@gmail.com>
2020-01-10 11:21:28 +0100

    fix post request

--- src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/BartolController.java
+++ src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/BartolController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +18,19 @@ public class BartolController {
     private UserRepository repository;
 
     @PostMapping("/users")
-    public void createUser(User user) {
+    public void createUser(@RequestBody User user) {
         repository.create(user);
         LOGGER.info("Successfully created user!");
+
+        LOGGER.info(user.getNationality());
+        LOGGER.info(user.getName().getFirst());
+        LOGGER.info(user.getName().getLast());
+        LOGGER.info(user.getGender());
+        LOGGER.info(user.getPhone());
+        LOGGER.info(String.valueOf(user.getId()));
+        LOGGER.info(user.getLocation().getCity());
+        LOGGER.info(user.getLocation().getCountry());
+
+
     }
 }
