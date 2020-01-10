dea12a0a1a7b504fcce1cb70ae0ca4b1b5ab9846
0036508924 <luka.lendel@fer.hr>
2020-01-10 11:28:09 +0100

    Finished task

--- src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/LukaController.java
+++ src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/LukaController.java
@@ -2,18 +2,18 @@ package hr.dreamfactory.lectures.soitbegins.controllers;
 
 import hr.dreamfactory.lectures.soitbegins.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.PathParam;
 
 @RestController
 public class LukaController {
+
     @Autowired
     private UserRepository repository;
 
-    @DeleteMapping("/users")
-    private void delete(@RequestParam Integer id) {
+    @DeleteMapping("/users/{id}")
+    private void delete(@PathVariable("id") Integer id) {
         repository.delete(id);
     }
 
