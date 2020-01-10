08fc765b147f4f4a66339694a2488c1034307568
Petra <petra.fribert@fer.hr>
2020-01-10 10:44:01 +0100

    changed endpoint

--- src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/PetraController.java
+++ src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/PetraController.java
@@ -1,15 +1,22 @@
 package hr.dreamfactory.lectures.soitbegins.controllers;
 
+import hr.dreamfactory.lectures.soitbegins.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 
 @RestController
 public class PetraController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Updated User";
+    @Autowired
+    private UserRepository repository;
+
+    @RequestMapping("/update/{id}")
+    public void update() {
+        repository.update(id, user);
     }
 }
