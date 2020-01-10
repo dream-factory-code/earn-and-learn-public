fd81dfb0a91012238ccd83c924d30aea99794360
0036508924 <luka.lendel@fer.hr>
2020-01-10 10:21:34 +0100

    Added delete method to controller

--- src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/LukaController.java
+++ src/main/java/hr/dreamfactory/lectures/soitbegins/controllers/LukaController.java
@@ -1,8 +1,19 @@
 package hr.dreamfactory.lectures.soitbegins.controllers;
 
+import hr.dreamfactory.lectures.soitbegins.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LukaController {
+    @Autowired
+    private UserRepository repository;
+
+    @RequestMapping("/delete")
+    private void delete(@RequestParam Integer id) {
+
+    }
 
 }
